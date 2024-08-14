8f2163f9b205da2b0098fe4137a08e823b0668e5
Bon <hyquang111@gmail.com>
2024-08-14 11:26:28 +0700

    Bon

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.printf("Hello Bui Nguyen Mai Bon!\n");
+        System.out.printf("Welcome to the DHKHMT18A!\n");
+    }
+}
